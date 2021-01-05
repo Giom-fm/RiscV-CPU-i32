2735e3cab3e8c30f2cb7fcd934210a244451efb5
Julian Weihe <julian.weihe@gmx.de>
2021-01-05 21:03:26 +0100

    test_utils funktion assert testet jetzt auch std_logic test_comparator angepasst ..hier ist noch n komischer Fehler

--- CPU/Design-Test/testbench/test_comparator.vhd
+++ CPU/Design-Test/testbench/test_comparator.vhd
@@ -14,7 +14,7 @@ architecture a_test_comparator of test_comparator is
     signal comparator_mode  : T_COMP_MODE;
     signal left             : std_logic_vector(31 downto 0);
     signal right            : std_logic_vector(31 downto 0);
-    signal pc_mode          : T_PC_SRC;
+    signal result          : std_logic;
 
 begin
 
@@ -22,131 +22,122 @@ begin
         i_comparator_mode   => comparator_mode,
         i_left              => left,
         i_right             => right,
-        o_pc_mode           => pc_mode
+        o_result           => result
         );
     
     test : process
     begin
+        compare_assert('0', '0', "Test1", C_DELAY);
+        compare_assert('0', '1', "Test2", C_DELAY);
+        compare_assert('1', '0', "Test3", C_DELAY);
+        compare_assert('1', '1', "Test4", C_DELAY);
 
-        -- TODO: das geht hier so nicht. :( .. wegen Paramtyp von compare_assert
-
-        -- Test: COMP_ALWAYS_ALU
-        comparator_mode <= COMP_ALWAYS_ALU;
-        left            <= "00000000000000000000000000000000";
-        right           <= "00000000000000000000000000000001";
-        compare_assert(pc_mode, PC_SRC_ALU, "COMP_ALWAYS_ALU", C_DELAY);
-
-        -- Test: COMP_ALWAYS_ADD
-        comparator_mode <= COMP_ALWAYS_ADD;
-        left            <= "00000000000000000000000000000000";
-        right           <= "00000000000000000000000000000001";
-        compare_assert(pc_mode, PC_SRC_ADD, "COMP_ALWAYS_ADD", C_DELAY);
 
         -- Test: COMP_EQUAL
         comparator_mode <= COMP_EQUAL;
         left            <= "01010101010101010101010101010101";
         right           <= "01010101010101010101010101010101";
-        compare_assert(pc_mode, PC_SRC_ALU, "COMP_EQUAL", C_DELAY);
+        compare_assert(result, '1', "COMP_EQUAL", C_DELAY);
 
         comparator_mode <= COMP_EQUAL;
         left            <= "01010101010101010101010101010101";
         right           <= "01010101010101010101010101010100";
-        compare_assert(pc_mode, PC_SRC_ADD, "COMP_EQUAL", C_DELAY);
+        compare_assert(result, '0', "COMP_EQUAL", C_DELAY);
 
         -- Test: COMP_NOT_EQUAL
         comparator_mode <= COMP_NOT_EQUAL;
         left            <= "01010101010101010101010101010101";
         right           <= "01010101010101010101010101010101";
-        compare_assert(pc_mode, PC_SRC_ADD, "COMP_NOT_EQUAL", C_DELAY);
+        compare_assert(result, '0', "COMP_NOT_EQUAL", C_DELAY);
 
         comparator_mode <= COMP_NOT_EQUAL;
         left            <= "01010101010101010101010101010101";
         right           <= "01010101010101010101010101010100";
-        compare_assert(pc_mode, PC_SRC_ALU, "COMP_NOT_EQUAL", C_DELAY);
+        compare_assert(result, '1', "COMP_NOT_EQUAL", C_DELAY);
 
         -- Test: COMP_LESS_THEN
         comparator_mode <= COMP_LESS_THEN;
         left            <= "00000000000000000000000000000000";
         right           <= "00000000000000000000000000000001";
-        compare_assert(pc_mode, PC_SRC_ALU, "COMP_LESS_THEN", C_DELAY);
+        compare_assert(result, '1', "COMP_LESS_THEN", C_DELAY);
 
         comparator_mode <= COMP_LESS_THEN;
         left            <= "00000000000000000000000000000001";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_PC, "COMP_LESS_THEN", C_DELAY);
+        compare_assert(result, '0', "COMP_LESS_THEN", C_DELAY);
 
         comparator_mode <= COMP_LESS_THEN;
         left            <= "00000000000000000000000000000000";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_PC, "COMP_LESS_THEN", C_DELAY);
+        compare_assert(result, '0', "COMP_LESS_THEN", C_DELAY);
 
         comparator_mode <= COMP_LESS_THEN;
         left            <= "11111111111111111111111111111111";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_ALU, "COMP_LESS_THEN", C_DELAY);
+        compare_assert(result, '1', "COMP_LESS_THEN", C_DELAY);
 
         -- Test: COMP_GREATER_EQUAL
         comparator_mode <= COMP_GREATER_EQUAL;
         left            <= "00000000000000000000000000000000";
         right           <= "00000000000000000000000000000001";
-        compare_assert(pc_mode, PC_SRC_PC, "COMP_GREATER_EQUAL", C_DELAY);
+        compare_assert(result, '0', "COMP_GREATER_EQUAL", C_DELAY);
 
         comparator_mode <= COMP_GREATER_EQUAL;
         left            <= "00000000000000000000000000000001";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_ADD, "COMP_GREATER_EQUAL", C_DELAY);
+        compare_assert(result, '0', "COMP_GREATER_EQUAL", C_DELAY);
 
         comparator_mode <= COMP_GREATER_EQUAL;
         left            <= "00000000000000000000000000000000";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_ADD, "COMP_GREATER_EQUAL", C_DELAY);
+        compare_assert(result, '0', "COMP_GREATER_EQUAL", C_DELAY);
 
         comparator_mode <= COMP_GREATER_EQUAL;
         left            <= "11111111111111111111111111111111";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_PC, "COMP_GREATER_EQUAL", C_DELAY);
+        compare_assert(result, '0', "COMP_GREATER_EQUAL", C_DELAY);
 
         -- Test: COMP_LESS_THEN_U
         comparator_mode <= COMP_LESS_THEN_U;
         left            <= "00000000000000000000000000000000";
         right           <= "00000000000000000000000000000001";
-        compare_assert(pc_mode, PC_SRC_ALU, "COMP_LESS_THEN_U", C_DELAY);
+        compare_assert(result, '1', "COMP_LESS_THEN_U", C_DELAY);
 
         comparator_mode <= COMP_LESS_THEN_U;
         left            <= "00000000000000000000000000000001";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_PC, "COMP_LESS_THEN_U", C_DELAY);
+        compare_assert(result, '0', "COMP_LESS_THEN_U", C_DELAY);
 
         comparator_mode <= COMP_LESS_THEN_U;
         left            <= "00000000000000000000000000000000";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_PC, "COMP_LESS_THEN_U", C_DELAY);
+        compare_assert(result, '0', "COMP_LESS_THEN_U", C_DELAY);
 
         comparator_mode <= COMP_LESS_THEN_U;
         left            <= "11111111111111111111111111111111";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_ADD, "COMP_LESS_THEN_U", C_DELAY);
+        compare_assert(result, '0', "COMP_LESS_THEN_U", C_DELAY);
 
         -- Test: COMP_GREATER_EQUAL_U
         comparator_mode <= COMP_GREATER_EQUAL_U;
         left            <= "00000000000000000000000000000000";
         right           <= "00000000000000000000000000000001";
-        compare_assert(pc_mode, PC_SRC_PC, "COMP_GREATER_EQUAL_U", C_DELAY);
+        compare_assert(result, '0', "COMP_GREATER_EQUAL_U", C_DELAY);
 
         comparator_mode <= COMP_GREATER_EQUAL_U;
         left            <= "00000000000000000000000000000001";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_ADD, "COMP_GREATER_EQUAL_U", C_DELAY);
+        compare_assert(result, '0', "COMP_GREATER_EQUAL_U", C_DELAY);
 
         comparator_mode <= COMP_GREATER_EQUAL_U;
         left            <= "00000000000000000000000000000000";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_ADD, "COMP_GREATER_EQUAL_U", C_DELAY);
+        compare_assert(result, '0', "COMP_GREATER_EQUAL_U", C_DELAY);
 
         comparator_mode <= COMP_GREATER_EQUAL_U;
         left            <= "11111111111111111111111111111111";
         right           <= "00000000000000000000000000000000";
-        compare_assert(pc_mode, PC_SRC_ALU, "COMP_GREATER_EQUAL_U", C_DELAY);
+        compare_assert(result, '1', "COMP_GREATER_EQUAL_U", C_DELAY);
 
         wait;
     end process;
--- CPU/Design-Test/testbench/test_utils.vhd
+++ CPU/Design-Test/testbench/test_utils.vhd
@@ -8,9 +8,18 @@ package test_utils is
     function to_string ( a: std_logic_vector)
         return string;
 
+    function to_string ( a: std_logic)
+        return string;
+
     procedure compare_assert(
         actual_data : std_logic_vector(31 downto 0);
         expect_data : std_logic_vector(31 downto 0);
+        report_msg  : string;
+        delay: time);
+    
+    procedure compare_assert(
+        actual_data : std_logic;
+        expect_data : std_logic;
         report_msg  : string;
         delay: time);
 
@@ -18,14 +27,21 @@ end package test_utils;
    
 package body test_utils is
 
-    function to_string ( a: std_logic_vector) return string is
+    function to_string (a: std_logic_vector) return string is
         variable b : string (1 to a'length) := (others => NUL);
         variable stri : integer := 1; 
         begin
             for i in a'range loop
-                b(stri) := std_logic'image(a((i)))(2);
+                b(stri) := std_logic'image(a(i))(2);
             stri := stri+1;
             end loop;
+        return b;
+    end function;
+
+    function to_string (a: std_logic) return string is
+        variable b : string (1 to 1) := (others => NUL);
+        begin
+            b(1) := std_logic'image(a)(2);
         return b;
     end function;
 
@@ -44,6 +60,25 @@ package body test_utils is
                 & "expect: [" & to_string(expect_data) & "], "
                 & "actual: [" & to_string(actual_data) & "]"
                 severity error;
+        end if;
+        wait for delay;
+    end procedure;
+
+    procedure compare_assert(
+        actual_data : std_logic;
+        expect_data : std_logic;
+        report_msg  : string;
+        delay: time)
+        is
+    begin
+        wait for delay;
+        if expect_data = actual_data then
+            report "PASSED " & report_msg severity note;
+        else
+            report "ASSERT " & report_msg & " -> "
+                & "expect: [" & to_string(expect_data) & "], "
+                & "actual: [" & to_string(actual_data) & "]"
+            severity error;
         end if;
         wait for delay;
     end procedure;
