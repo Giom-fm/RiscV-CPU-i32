5ab9039b9ef6756729f13b9775176bacbf37d3cb
Guillaume Fournier-Mayer <guillaume.fm@mailbox.org>
2021-03-15 13:17:15 +0100

    Add inline documentation for "Utils"

--- CPU/Design/src/utils/types.vhd
+++ CPU/Design/src/utils/types.vhd
@@ -1,70 +1,84 @@
+-- Defines types for the modules
+-- Author: Guillaume Fournier-Mayer
+
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 
 package types is
 
-	constant PC_ADD 			: integer := 4;
+  -- The value to be added to the current program counter
+  constant PC_ADD : integer := 4;
 
-	type T_ALU_MODE is (
-		ALU_UNUSED,
-		ALU_ADD,
-		ALU_ADD_EVEN,
-		ALU_SUB,
-		ALU_SLL,
-		ALU_SLT,
-		ALU_SLTU,
-		ALU_XOR,
-		ALU_SRL,
-		ALU_SRA,
-		ALU_OR,
-		ALU_AND
-		);
+  -- The operation that the ALU should perform
+  type T_ALU_MODE is (
+    ALU_UNUSED,
+    ALU_ADD,
+    ALU_ADD_EVEN,
+    ALU_SUB,
+    ALU_SLL,
+    ALU_SLT,
+    ALU_SLTU,
+    ALU_XOR,
+    ALU_SRL,
+    ALU_SRA,
+    ALU_OR,
+    ALU_AND
+  );
 
-	type T_SEXT_MEM_MODE is (
-		SEXT_MEM_S_8,
-		SEXT_MEM_S_16,
-		SEXT_MEM_U_8,
-		SEXT_MEM_U_16,
-		SEXT_MEM_32
-	);
+  -- Specifies how the 32 bits are to be filled up
+  type T_SEXT_MEM_MODE is (
+    SEXT_MEM_S_8,
+    SEXT_MEM_S_16,
+    SEXT_MEM_U_8,
+    SEXT_MEM_U_16,
+    SEXT_MEM_32
+  );
 
-	type T_STORE_MODE is (
-		STORE_B,
-		STORE_H,
-		STORE_W
-	);
+  -- The datawidth of the data that should be stored in memory
+  type T_STORE_MODE is (
+    STORE_B,
+    STORE_H,
+    STORE_W
+  );
 
-	type T_MUX_ALU is (
-		MUX_ALU_RS1_RS2,
-		MUX_ALU_RS1_IMM,
-		MUX_ALU_PC_IMM,
-		MUX_ALU_PC_RS2
-	);
+  -- The mode of the ALU multiplexer
+  -- Decides which value is pushed into the ALU
+  type T_MUX_ALU is (
+    MUX_ALU_RS1_RS2,
+    MUX_ALU_RS1_IMM,
+    MUX_ALU_PC_IMM,
+    MUX_ALU_PC_RS2
+  );
 
-	type T_MUX_REG is (
-		MUX_REG_ZERO,
-		MUX_REG_ALU,
-		MUX_REG_MEM,
-		MUX_REG_PC
-	);
+  -- The mode of the register multiplexer
+  -- Decides which value is pushed into the destination register (rd)
+  type T_MUX_REG is (
+    MUX_REG_ZERO,
+    MUX_REG_ALU,
+    MUX_REG_MEM,
+    MUX_REG_PC
+  );
 
-	type T_PC_MODE is (
-		PC_SRC_ADD,
-		PC_SRC_COMP_ALU,
-		PC_SRC_ALU
-	);
+  -- The source from which the program counter is added or set
+  type T_PC_MODE is (
+    PC_SRC_ADD,
+    PC_SRC_COMP_ALU,
+    PC_SRC_ALU
+  );
 
-	type T_COMP_MODE is (
-		COMP_EQUAL,
-		COMP_NOT_EQUAL,
-		COMP_LESS_THEN,
-		COMP_GREATER_EQUAL,
-		COMP_LESS_THEN_U,
-		COMP_GREATER_EQUAL_U
-	);
+  -- The operation that the comparator should perform
+  type T_COMP_MODE is (
+    COMP_EQUAL,
+    COMP_NOT_EQUAL,
+    COMP_LESS_THEN,
+    COMP_GREATER_EQUAL,
+    COMP_LESS_THEN_U,
+    COMP_GREATER_EQUAL_U
+  );
 
-	type T_MEM_DIR is (
-		MEM_DIR_READ,
-		MEM_DIR_WRITE
-	);
+  -- Specifies whether the access to the ram should be read or write
+  type T_MEM_DIR is (
+    MEM_DIR_READ,
+    MEM_DIR_WRITE
+  );
 end package;
--- CPU/Design/src/utils/utils.vhd
+++ CPU/Design/src/utils/utils.vhd
@@ -1,40 +1,49 @@
+-- The module provides functions for recurring tasks
+-- Author: Guillaume Fournier-Mayer
+
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
 use work.types.all;
 
 package utils is
 
-    function extend(data: std_logic_vector; length: integer)
-        return std_logic_vector;
+  -- Fills up the upper remaining bits with zeros
+  -- data: The data to be filled up
+  -- length: The length to be filled up
+  function extend(data : std_logic_vector; length : integer
+  ) return std_logic_vector;
 
-    function extend_signed(data: std_logic_vector; length: integer)
-        return std_logic_vector;
+  -- Fills up the upper remaining bits with ones
+  -- data: The data to be filled up
+  -- length: The length to be filled up
+  function extend_signed(data : std_logic_vector; length : integer
+  ) return std_logic_vector;
 
-    function bool_to_word(bool: boolean)
-        return std_logic_vector;
+  -- Converts a boolean value into a std_logic_vector
+  function bool_to_word(bool : boolean
+  ) return std_logic_vector;
 
 end package utils;
-   
+
 package body utils is
 
-    function extend(data: std_logic_vector; length: integer) return std_logic_vector is
-    begin
-        return std_logic_vector(resize(unsigned(data), length));
-    end;
+  function extend(data : std_logic_vector; length : integer) return std_logic_vector is
+  begin
+    return std_logic_vector(resize(unsigned(data), length));
+  end;
 
-    function extend_signed(data: std_logic_vector; length: integer) return std_logic_vector is
-    begin
-        return std_logic_vector(resize(signed(data), length));
-    end;
+  function extend_signed(data : std_logic_vector; length : integer) return std_logic_vector is
+  begin
+    return std_logic_vector(resize(signed(data), length));
+  end;
 
-    function bool_to_word(bool: boolean) return std_logic_vector is
-    begin
-        if(bool) then
-            return extend("1", 32);
-        else 
-            return extend("0", 32);
-        end if;
-    end;
- end package body utils;
- 
+  function bool_to_word(bool : boolean) return std_logic_vector is
+  begin
+    if (bool) then
+      return extend("1", 32);
+    else
+      return extend("0", 32);
+    end if;
+  end;
+end package body utils;
